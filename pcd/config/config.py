5de7e81ce9d02e5ca44b9e9fbaac151174a87959
rdodkins <rupertdodkins@gmail.com>
2020-01-07 11:48:50 -0800

    Paths automatically update when system is changed using environment variables

--- pcd/config/config.py
+++ pcd/config/config.py
@@ -1,5 +1,31 @@
 import os
 import yaml
+import re
+
+# because pycharm doesn't read the environment variables  so this sets the automatically
+# for quick switching between machines
+from os.path import expanduser
+home = expanduser("~")
+if home == '/Users/dodkins':
+    os.environ["WORKING_DIR"] = "/Users/dodkins"
+elif home == '/home/dodkins':
+    os.environ["WORKING_DIR"] = "/mnt/data0/dodkins"
+else:
+    print('System not recognised. Make sure $WORKING_DIR is set')
+
+path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+def path_constructor(loader, node):
+  ''' Extract the matched value, expand env variable, and replace the match '''
+  value = node.value
+  match = path_matcher.match(value)
+  env_var = match.group()[2:-1]
+  var = os.environ.get(env_var)
+  if not var:
+      print(f'No environment variable with name {env_var} found')
+  return var + value[match.end():]
+
+yaml.add_implicit_resolver('!path', path_matcher)
+yaml.add_constructor('!path', path_constructor)
 
 ## define custom tag handler
 def join(loader, node):
@@ -15,5 +41,5 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.yml"
     except yaml.YAMLError as exc:
         config = exc
 
-
-print(config)
+if __name__ == '__main__':
+    print(config)
