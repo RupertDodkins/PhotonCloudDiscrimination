18048052f33d1440a2520526f8192640b910a164
rdodkins <rupertdodkins@gmail.com>
2020-01-21 12:13:38 -0800

    Automated window scaling for init_grid function

--- pcd/utils.py
+++ pcd/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def init_grid(rows, cols):
     gs = gridspec.GridSpec(rows, cols)
-    fig = plt.figure(figsize=(12, 6), constrained_layout=True)
+    fig = plt.figure(figsize=(12, rows*3), constrained_layout=True)
     axes = []
     for i in range(rows):
         for j in range(cols):
